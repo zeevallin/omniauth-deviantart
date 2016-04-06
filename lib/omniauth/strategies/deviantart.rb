a45839340a21399f75debac72b1ce6a4d8dba40f
klpl <us.kenneth@gmail.com>
2016-04-06 16:42:58 -0700

    update urls

--- lib/omniauth/strategies/deviantart.rb
+++ lib/omniauth/strategies/deviantart.rb
@@ -5,26 +5,27 @@ module OmniAuth
     class DeviantArt < OmniAuth::Strategies::OAuth2
       
       option :client_options, {
-        :site => 'https://www.deviantart.com/oauth2/draft15',
-        :authorize_url => 'https://www.deviantart.com/oauth2/draft15/authorize',
-        :token_url => 'https://www.deviantart.com/oauth2/draft15/token'
+        :site => 'https://www.deviantart.com/oauth2',
+        :authorize_url => 'https://www.deviantart.com/oauth2/authorize',
+        :token_url => 'https://www.deviantart.com/oauth2/token'
       }
 
       uid { raw_info['username'] }
       
       info do
         {
+          'name' => raw_info['profile']['real_name'],
           'nickname' => raw_info['username'],
           'image' => raw_info['usericonurl']
         }
       end
-            
+
       extra do
         { :raw_info => raw_info }
       end
       
       def raw_info
-        @raw_info ||= access_token.get('https://www.deviantart.com/api/draft15/user/whoami', params: { token: access_token.token }).parsed
+        @raw_info ||= access_token.get('https://www.deviantart.com/api/v1/oauth2/user/whoami', params: { token: access_token.token }).parsed
       end
       
     end
